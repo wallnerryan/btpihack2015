8f4167d2fa40e922cbc57df8cdd0692a2ac0847d
Ryan Wallner <wallnerryan@gmail.com>
2015-07-24 16:59:23 +0200

    adding load data script
    Signed-off-by: Ryan Wallner <wallnerryan@gmail.com>

--- tools/load_data.py
+++ tools/load_data.py
@@ -3,8 +3,31 @@
 # Load data into an existing metadata container
 
 """
-load_data -f file.txt -c container-1
-load_data -s "This is my string" -c container-1
+load_data -f /path/to/file.txt -o /container/path/ -c container-1
 """
 
+import optparse
+import os
+import subprocess
+import re
+       
+def main():
+  p = optparse.OptionParser()
+  p.add_option('--file', '-f', default="data.txt")
+  p.add_option('--container', '-c', default="container")
+  p.add_option('--cpath', '-o', default="/home/")
+  p.add_option('--name', '-n', default="received_data")
+  options, arguments = p.parse_args()
+  print 'Loading %s' % options.file
+  print 'Into %s' % options.container
+  print 'At %s%s' % (options.cpath, options.name)
 
+  
+  os.system('docker exec %s bash -c "nc -l 4444 > %s%s" &' % (options.container, options.cpath, options.name))  
+  proc = subprocess.Popen(["docker inspect %s | grep -i ipaddress" % options.container], stdout=subprocess.PIPE, shell=True)
+  (out, err) = proc.communicate()
+  ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', out )[0]
+  os.system('netcat %s 4444 < %s' % (ip, options.file))
+         
+if __name__ == '__main__':
+  main()
